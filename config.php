32e6ef35fe653531e5164caaf788b6a65b4b8509
hengvenchang <cheangvenheng@gmail.com>
2026-04-06 12:03:46 +0700

    Test SQL connection

--- config.php
+++ config.php
@@ -10,11 +10,9 @@
  */
 
 // Database connection settings
-define('DB_HOST', getenv('MYSQLHOST') ?: '');       // fallback empty string
-define('DB_USER', getenv('MYSQLUSER') ?: '');
-define('DB_PASS', getenv('MYSQLPASSWORD') ?: '');
-define('DB_NAME', getenv('MYSQLDATABASE') ?: '');// Database name
+define('DB_HOST', 'mysql.railway.internal');
+define('DB_USER', 'root');
+define('DB_PASS', 'CXXQhBuiOkAcyUQWgtwiaxMxLZjpkzVS');
+define('DB_NAME', 'railway');
 
-// Railway injects MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLDATABASE
-// For other providers, set DB_HOST, DB_USER, DB_PASS, DB_NAME in your environment
 ?>
